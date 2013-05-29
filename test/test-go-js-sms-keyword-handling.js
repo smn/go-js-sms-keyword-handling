882ddb28b8de44ec70990982d80249d92260a529
Simon de Haan <simon@praekeltfoundation.org>
2013-05-29 15:58:27 +0200

    KeywordState

--- test/test-go-js-sms-keyword-handling.js
+++ test/test-go-js-sms-keyword-handling.js
@@ -23,15 +23,36 @@ describe("SMS keyword application", function() {
         async: true
     });
 
-    it('should increment the key and end the session', function(done) {
+    it('should accept "red" as a keyword and transition to a state', function(done) {
         var p = tester.check_state({
             user: null,
-            content: null,
-            next_state: 'done',  // it's only 1 state that we return to,
-            response: 'You are visitor number 1',
+            content: 'red',
+            next_state: 'red',
+            response: 'You chose red!',
             continue_session: false
         });
         p.then(done, done);
     });
 
+    it('should accept "green" as a keyword and transition to a state', function(done) {
+        var p = tester.check_state({
+            user: null,
+            content: 'green',
+            next_state: 'green',
+            response: 'You chose green!',
+            continue_session: false
+        });
+        p.then(done, done);
+    });
+
+    it('should complain about "blue" as a keyword', function(done) {
+        var p = tester.check_state({
+            user: null,
+            content: 'blue',
+            next_state: 'start',
+            response: 'Invalid keyword provided',
+            continue_session: true
+        });
+        p.then(done, done);
+    });
 });
